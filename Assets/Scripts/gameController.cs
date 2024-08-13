33e33d58da1f471a6991a35803abf1e5a17a951f
Pons Anthony Advento <adventopr@students.nu-fairview.edu.ph>
2024-08-13 20:08:05 +0800

    variable declarations

--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int playerTurn;
+    public int playerTurnCount;
+    // public GameObject[] playerTurnIcons;
+    public Sprite[] playerTurnIcons;
+    public Button[] boardButtons;
     void Start()
     {
         
